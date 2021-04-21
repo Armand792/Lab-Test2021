9fc88325386e20a873250273716b3a862fba050c
Armando Cardozo <armandopatricio704@hotmail.com>
2021-04-21 19:07:18 +0100

    printscore updated

--- java/src/ie/tudublin/ScoreDisplay.java
+++ java/src/ie/tudublin/ScoreDisplay.java
@@ -8,20 +8,15 @@ import processing.core.PApplet;
 
 public class ScoreDisplay extends PApplet
 {
-	String score = "DEFGABcd";
+	//String score = "DEFGABcd";
 	//String score = "D2E2F2G2A2B2c2d2";
-	//String score = "DEF2F2F2EFA2A2B2AFD2E2D2D2D2";
+	String score = "DEF2F2F2EFA2A2B2AFD2E2D2D2D2";
 
 	ArrayList<Note> notes;
 	
 	public void settings()
 	{
 		size(1000, 500);
-
-		// How to convert a character to a number
-		char c = '7'; // c holds the character 7 (55)
-		int i = c - '0'; // i holds the number 7 (55 - 48) 
-		println(i);
 	}
 
 	public void setup() 
@@ -62,8 +57,13 @@ public class ScoreDisplay extends PApplet
 
 	void printScore(){
 		for (Note note:notes){
-			println(note.getNote() + " " + note.getDuration());
-
+			System.out.printf(note.getNote() + " " + note.getDuration() );
+			if(note.getDuration() == 1){
+				System.out.println(" Quaver");
+			}
+			else{
+				System.out.println(" Crotchet");
+			}s
 		}
 	}
 
